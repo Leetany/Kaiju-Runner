f4ff6235b485120bdb36c77ae6bc4a065eab12d0
2Rion630 <hank92315@gmail.com>
2025-07-08 16:33:17 +0900

    Run

---
+++ Assets/Test_Folder/Scripts/ItemBonusTime.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ItemBonusTime : MonoBehaviour
+{
+    public float bonusTime = 3f;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            
+            GameManager gameManager = Object.FindFirstObjectByType<GameManager>();
+            if (gameManager != null)
+            {
+                gameManager.AddBonusTime(bonusTime);
+            }
+            Destroy(gameObject); // 아이템 제거
+        }
+    }
+}
