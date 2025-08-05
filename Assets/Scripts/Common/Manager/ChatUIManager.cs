9647f115046cb185d9f6c2973f242ac4c845223b
Leetany <wlsdh4883@gmail.com>
2025-08-05 10:17:05 +0900

    .

---
+++ Assets/Scripts/Common/Manager/ChatUIManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ChatUIManager : MonoBehaviour
+{
+    public static ChatUIManager Instance;
+
+    public GameObject chatPrefab;
+    public GameObject Parent;
+
+
+    private void Awake()
+    {
+        Instance = this;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.O))
+        {
+            Instantiate(chatPrefab, Parent.transform);
+        }
+    }
+}
